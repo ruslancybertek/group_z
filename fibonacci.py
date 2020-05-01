4fa1fc2a2cb8d14126bc5852706cc429250190dd
Ruslan Huseynov <ruslanhuseynov@Ruslans-MacBook-Pro.local>
2020-05-01 17:10:26 -0400

    group-z

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+target = 10
+n1, n2 = 1, 1
+count = 0
+num_list = []
+if target <= 0:
+   print("Please enter a positive integer")
+elif target == 1:
+   print("Fibonacci sequence upto",target,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < target:
+       num_list.append(n1)
+       summ = n1 + n2
+       n1 = n2
+       n2 = summ
+       count += 1
+print(num_list)
---
+++ mainPage.py
@@ -0,0 +1,8 @@
+lis = []
+for num in range(2,100):
+    for i in range(2,num):
+        if (num % i) == 0:
+            break
+    else:
+        lis.append(num)
+print(lis)
